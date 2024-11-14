57f749b5fc1757d3432a9ea81381a6461eb5e6d1
AliPaknejad2 <alipaknejad78@gmail.com>
2024-11-14 17:31:03 +0330

    'v4'

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+ 
+        <div>
+            <p id="fd">BMW</p>
+            
+        </div>
+    </header>
+    <nav>
+        
+    </nav>
+    <main>
+        <div id="m1">
+            <ul>
+                <li>صفحه اصلی</li>
+                <li>ورود</li>
+                <li>درباره ما</li>
+                <li>محصولات</li>
+            </ul>
+        </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bmw.css">
+</head>
+<body>
+        <?php 
+            include("includes/header.php");
+        ?>
+   
+        <div id="m2">
+            <div id="mini">
+                <dd>
+                    نام خودرو : بی ام و 220i گرن کوپه
+                    <br>
+                    حجم موتور : 1997 سی سی 
+                    <br>
+                    
+                    کلاس بدنه : سدان
+                    <br>
+                    نوع موتور : بنزینی دارای توربو
+                    <br>
+                    تعداد سیلندرها : 4 سیلندر
+                    <br>
+                    نوع جعبه دنده : اتوماتیک دوکلاچه
+                    <br>
+                    مصرف سوخت ترکیبی : 6 لیتر در 100 کیلو متر
+                    <br>
+                    شتاب 0 تا 100 : 7.1 ثانیه
+                    <br>
+                    حداکثر سرعت (km/h) : 238
+                    <br>
+                    <br>
+                    <a href="220i.html">اطلاعات بیشتر</a>
+    
+                    
+                </dd>
+                <img src="220i گرن کوپه - 1K JPEG.jpg">
+            </div>
+            <div id="mini2">
+                <dd>
+                    نام خودرو : بی ام و 220i گرن کوپه
+                    <br>
+                    حجم موتور : 1997 سی سی 
+                    <br>
+                    
+                    کلاس بدنه : سدان
+                    <br>
+                    نوع موتور : بنزینی دارای توربو
+                    <br>
+                      تعداد سیلندرها : 4 سیلندر
+                    <br>
+                    نوع جعبه دنده : اتوماتیک دوکلاچه
+                    <br>
+                    مصرف سوخت ترکیبی : 6 لیتر در 100 کیلو متر
+                    <br>
+                    شتاب 0 تا 100 : 7.1 ثانیه
+                    <br>
+                    حداکثر سرعت (km/h) : 238
+                    <br>
+                    <br>
+                </dd>
+                <img src="640i - 1K JPEG.jpg">
+            </div>
+        </div>
+        
+        <?php 
+            include("includes/footer.php");
+        ?>
+   
+            
+
+</body>
+</html>
+<!-- <div style="background:pink; padding:10px; overflow:auto;">
+ <img src="https://adminesite.com/wp-content/uploads/2020/06/css-floats-300x206.png" alt="CSS floats" style="float:right; margin-left:10px;">
+ <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
+</div>
+ -->
